7080d3dcc0c03cd521c3a3fbdb44e2083f60d59f
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-06-04 23:10:39 +0530

    Create firebase-config.js

---
+++ f-shell/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdRkcr4Ajo9XAaOBBflowilmNXC-3BQ9g",
+  authDomain: "fshell.firebaseapp.com",
+  databaseURL: "https://fshell-default-rtdb.firebaseio.com",
+  projectId: "fshell",
+  storageBucket: "fshell.appspot.com",
+  messagingSenderId: "565684073332",
+  appId: "1:565684073332:web:14c28a56e17c8274e4b221"
+};
+
+firebase.initializeApp(firebaseConfig);
